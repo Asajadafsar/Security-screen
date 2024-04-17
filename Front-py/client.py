73fd6ed4d04a3bf12c36b8fe1f8dc0f86d7f8383
Asajadafsar <smyhqasmyan2@gmail.com>
2024-04-17 05:02:51 +0430

    finale cilent

--- Front-py/client.py
+++ Front-py/client.py
@@ -1,37 +1,57 @@
+import asyncio
+import websockets
 import subprocess
 import numpy as np
 from mss import mss
 import cv2
 
-# Settings for screen recording
+# Screen recording settings
 sct = mss()
-# Getting information about the first monitor (usually the primary monitor)
-monitor = sct.monitors[1]  # Index 1 represents the primary monitor
-monitor_info = {'top': monitor['top'], 'left': monitor['left'], 'width': monitor['width'], 'height': monitor['height']}
+monitor = sct.monitors[1]  # First monitor
 
-# FFmpeg settings for streaming data to the server
-command = [
-    'ffmpeg',
-    '-y',  # Overwrite existing files
-    '-f', 'rawvideo',  # Input format is raw video
-    '-vcodec', 'rawvideo',  # Input video codec
-    '-pix_fmt', 'bgr24',  # Input pixel format
-    '-s', "{}x{}".format(monitor_info['width'], monitor_info['height']),  # Using monitor dimensions
-    '-r', '10',  # Frame rate
-    '-i', '-',  # Read data from stdin
-    '-c:v', 'libx264',  # Output video codec
-    '-pix_fmt', 'yuv420p',  # Output pixel format
-    '-preset', 'ultrafast',  # Default settings for high speed
-    '-f', 'flv',  # Output format
-    'rtmp://server_address/live/stream_key'  # RTMP server address
-]
+async def record_screen(start_recording):
+    """Record the screen and send data to the server using FFmpeg."""
+    command = [
+        'ffmpeg',
+        '-y',
+        '-f', 'rawvideo',
+        '-vcodec', 'rawvideo',
+        '-pix_fmt', 'bgr24',
+        '-s', "{}x{}".format(monitor['width'], monitor['height']),
+        '-r', '10',
+        '-i', '-',
+        '-c:v', 'libx264',
+        '-pix_fmt', 'yuv420p',
+        '-preset', 'ultrafast',
+        '-f', 'flv',
+        'rtmp://server_address/live/stream_key'
+    ]
+    pipe = subprocess.Popen(command, stdin=subprocess.PIPE)
 
-# Running the FFmpeg process
-pipe = subprocess.Popen(command, stdin=subprocess.PIPE)
+    while start_recording:
+        img = sct.grab(monitor)
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+        pipe.stdin.write(frame.tobytes())
 
-while True:
-    # Capturing the screen
-    img = sct.grab(monitor_info)
-    frame = np.array(img)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)  # Convert color format
-    pipe.stdin.write(frame.tobytes())  # Writing frame to stdin for FFmpeg
+    pipe.terminate()
+
+async def websocket_client():
+    uri = "ws://localhost:8765"  # Websocket server address
+    async with websockets.connect(uri) as websocket:
+        start_recording = False
+        while True:
+            message = await websocket.recv()
+            if message == "start":
+                if not start_recording:
+                    start_recording = True
+                    asyncio.create_task(record_screen(start_recording))
+                    print("Recording started.")
+            elif message == "stop":
+                if start_recording:
+                    start_recording = False
+                    print("Recording stopped.")
+            else:
+                print(f"Unknown command: {message}")
+
+asyncio.run(websocket_client())
